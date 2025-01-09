661e4c1bf631fa7871f11ed29d583bd9bcc60cfb
sharif-minhaz <minhaz.rbs@gmail.com>
2025-01-09 15:14:07 +0600

    feat(options): add options param

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { INestApplication } from "@nestjs/common";
+import { HttpMethod } from "types";
 
 /**
  * Logs all registered routes in a NestJS application.
  * @param app - The NestJS application instance (must implement INestApplication).
+ * @param options - Configuration options for logging routes.
+ * @param options.ignoreMethods - An array of HTTP methods to ignore when logging routes. This helps filter out specific HTTP methods like 'GET', 'POST', etc.
+ * @returns {void}
  */
-export function logRegisteredRoutes(app: INestApplication<any>) {
+export function logRegisteredRoutes(
+	app: INestApplication<any>,
+	{ ignoreMethods }: { ignoreMethods: HttpMethod[] }
+) {
+	// Log all registered routes
 	const server = app.getHttpServer();
 
 	const router = server._events?.request?._router;
@@ -16,9 +24,15 @@ export function logRegisteredRoutes(app: INestApplication<any>) {
 	}
 
 	const routes = router.stack
-		.filter((layer: any) => layer.route) // Only consider routes
+		.filter(
+			(layer: any) =>
+				layer.route &&
+				!ignoreMethods.includes(
+					Object.keys(layer.route?.methods || {})[0].toLowerCase() as HttpMethod
+				)
+		)
 		.map((layer: any) => ({
-			method: Object.keys(layer.route.methods)[0].toUpperCase(),
+			method: Object.keys(layer.route?.methods)[0].toUpperCase(),
 			path: layer.route.path,
 		}));
 
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,20 @@
 import { INestApplication } from "@nestjs/common";
 
+type HttpMethod =
+	| "get"
+	| "post"
+	| "put"
+	| "delete"
+	| "patch"
+	| "options"
+	| "head"
+	| "acl"
+	| "trace"
+	| "connect";
+
 declare module "nestjs-routes-viewer" {
-	export function logRegisteredRoutes(app: INestApplication<any>): void;
+	export function logRegisteredRoutes(
+		app: INestApplication<any>,
+		{ ignoreMethods }: { ignoreMethods: HttpMethod[] }
+	): void;
 }
