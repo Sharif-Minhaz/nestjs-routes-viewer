47ed79e9e7bb6e46dd9ff9b2b38dc88223bdf628
sharif-minhaz <minhaz.rbs@gmail.com>
2025-01-17 19:07:15 +0600

    build(option): build showGuards column option

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ exports.logRegisteredRoutes = logRegisteredRoutes;
  * @param app - The NestJS application instance (must implement INestApplication).
  * @param options - Configuration options for logging routes. (Optional)
  * @param options.ignoreMethods - An array of HTTP methods to ignore when logging routes. This helps filter out specific HTTP methods like 'get', 'post', etc (lowercase). (Optional)
+ * @param options.showGuards - A boolean indicating whether to include guards column in the route logs, default is false.
  * @returns {void}
  */
-function logRegisteredRoutes(app, { ignoreMethods } = {}) {
+function logRegisteredRoutes(app, { ignoreMethods, showGuards = false }) {
     var _a, _b;
     const methodsToIgnore = ignoreMethods || [];
-    // Log all registered routes
     const server = app.getHttpServer();
     const router = (_b = (_a = server._events) === null || _a === void 0 ? void 0 : _a.request) === null || _b === void 0 ? void 0 : _b._router;
     if (!router || !router.stack) {
-        console.error("Router is not available. Ensure the application uses a compatible platform like Express.");
+        console.error("Router is not available. Ensure the application uses Express.");
         return;
     }
     const routes = router.stack
@@ -26,11 +26,29 @@ function logRegisteredRoutes(app, { ignoreMethods } = {}) {
     })
         .map((layer) => {
         var _a;
-        return ({
-            method: Object.keys((_a = layer.route) === null || _a === void 0 ? void 0 : _a.methods)[0].toUpperCase(),
-            path: layer.route.path,
-        });
+        const route = layer.route;
+        const handler = (_a = route === null || route === void 0 ? void 0 : route.stack[0]) === null || _a === void 0 ? void 0 : _a.handle;
+        // Get guards
+        const guards = Reflect.getMetadata("__guards__", handler) || [];
+        const guardNames = guards
+            .map((guard) => guard.name || "Unknown")
+            .filter((name) => name !== "Unknown");
+        return {
+            method: Object.keys(route.methods)[0].toUpperCase(),
+            path: route.path,
+            guards: guardNames,
+        };
+    });
+    const formattedRoutes = routes.map((route) => {
+        const formattedRoute = {
+            method: route.method,
+            path: route.path,
+        };
+        if (showGuards) {
+            formattedRoute.guards = route.guards.join(", ") || "None";
+        }
+        return formattedRoute;
     });
     console.log("Registered Routes:");
-    console.table(routes);
+    console.table(formattedRoutes);
 }
